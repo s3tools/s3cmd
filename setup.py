f81e7fba9628fb3732f9862e5832c05f83d42915
Michal Ludvig <mludvig@logix.net.nz>
2007-01-13 13:19:43 +0000

    All classes from s3.py go to S3/S3.py Added setup.py
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3py/trunk@45 830e0280-6d2a-0410-9c65-932aecc39d9d

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name = "s3cmd",
+	version = "0.9.0a1",
+	author = "Michal Ludvig",
+	author_email = "michal@logix.cz",
+	packages = [ 'S3' ],
+	scripts = ['s3.py'],
+	)
