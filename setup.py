7023e93151a2c06dfef3e1160cf78b50ac6483bd
Michal Ludvig <mludvig@logix.net.nz>
2007-01-19 00:11:11 +0000

    Added info for building RPM packages.
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3py/trunk@54 830e0280-6d2a-0410-9c65-932aecc39d9d

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from distutils.core import setup
 setup(name = "s3cmd",
+	## Content description
 	version = "0.9.0a1",
-	author = "Michal Ludvig",
-	author_email = "michal@logix.cz",
 	packages = [ 'S3' ],
 	scripts = ['s3cmd'],
+
+	## Packaging details
+	author = "Michal Ludvig",
+	author_email = "michal@logix.cz",
+	url = 'http://s3tools.sourceforge.net',
+	license = 'GPL version 2',
+	description = 'S3cmd is a tool for managing your Amazon S3 storage.',
+	long_description = """
+S3cmd lets you copy files from/to Amazon S3 
+(Simple Storage Service) using a simple to use
+command line client.
+
+Authors:
+--------
+    Michal Ludvig <michal@logix.cz>
+""",
 	)
