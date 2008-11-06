85055a7c359aba8546dad4db2992e7db27c1920d
Michal Ludvig <mludvig@logix.net.nz>
2008-11-06 06:23:15 +0000

    * run-tests.sh: Use unique bucket name for testing. Apparently someone   else snatched my favourite s3://s3cmd-autotest :-D
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3cmd/branches/0.9.8.x@250 830e0280-6d2a-0410-9c65-932aecc39d9d

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e -x
 
-./s3cmd mb s3://s3cmd-autotest
-./s3cmd ls s3://s3cmd-autotest
-./s3cmd put s3cmd s3cmd.1 s3://s3cmd-autotest
-./s3cmd ls s3://s3cmd-autotest
-./s3cmd del s3://s3cmd-autotest/s3cmd.1
-./s3cmd get s3://s3cmd-autotest/s3cmd s3cmd.get
+TEST_BUCKET="s3://s3cmd-autotest-$(id -un)-$(date +%s)"
+echo "Using test bucket: ${TEST_BUCKET}"
+echo
+sleep 1
+
+./s3cmd mb ${TEST_BUCKET}
+./s3cmd ls ${TEST_BUCKET}
+./s3cmd put s3cmd s3cmd.1 ${TEST_BUCKET}
+./s3cmd ls ${TEST_BUCKET}
+./s3cmd del ${TEST_BUCKET}/s3cmd.1
+./s3cmd get ${TEST_BUCKET}/s3cmd s3cmd.get
 diff s3cmd s3cmd.get
 rm -fv s3cmd.get
 
@@ -19,24 +24,24 @@ set -x
 VER=$(./s3cmd --version | cut -d\  -f3)
 tar xvfz dist/s3cmd-${VER}.tar.gz
 echo "Will be removed" > s3cmd-${VER}/file.to.remove
-./s3cmd sync s3cmd-${VER} s3://s3cmd-autotest/sync-test
+./s3cmd sync s3cmd-${VER} ${TEST_BUCKET}/sync-test
 echo "Added file" > s3cmd-${VER}/added.file
 rm -f s3cmd-${VER}/file.to.remove
-./s3cmd sync --delete s3cmd-${VER} s3://s3cmd-autotest/sync-test
+./s3cmd sync --delete s3cmd-${VER} ${TEST_BUCKET}/sync-test
 rm -f s3cmd-${VER}/S3/PkgInfo.py
 rm -f s3cmd-${VER}/s3cmd
-./s3cmd sync --delete --exclude "/s3cmd-${VER}/S3/*" s3://s3cmd-autotest/sync-test s3cmd-${VER}
+./s3cmd sync --delete --exclude "/s3cmd-${VER}/S3/*" ${TEST_BUCKET}/sync-test s3cmd-${VER}
 rm -rf s3cmd-${VER}
 
-./s3cmd rb s3://s3cmd-autotest/ || true
+./s3cmd rb ${TEST_BUCKET}/ || true
 # ERROR: S3 error: 409 (Conflict): BucketNotEmpty
 
 # hack to remove all objects from a bucket
 mkdir empty
-./s3cmd sync --delete empty/ s3://s3cmd-autotest
+./s3cmd sync --delete empty/ ${TEST_BUCKET}
 rm -rf empty
 
-./s3cmd rb s3://s3cmd-autotest/
+./s3cmd rb ${TEST_BUCKET}/
 
 set +x
 
