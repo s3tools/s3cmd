c29ee86d4fb2ae941a64938f45b91c65a07b12ea
Michal Ludvig <mludvig@logix.net.nz>
2008-11-30 14:20:20 +0000

    * Released version 0.9.9-pre3 	  ---------------------------
    	* S3/PkgInfo.py: Bumped up version to 0.9.9-pre3
    
    
    
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3cmd/trunk@279 830e0280-6d2a-0410-9c65-932aecc39d9d

--- S3/PkgInfo.py
+++ S3/PkgInfo.py
@@ -1,5 +1,5 @@
 package = "s3cmd"
-version = "0.9.9-pre2+svn"
+version = "0.9.9-pre3"
 url = "http://s3tools.logix.cz"
 license = "GPL version 2"
 short_description = "S3cmd is a tool for managing Amazon S3 storage space."
