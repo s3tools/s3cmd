d07b947c0de73d4a7aac128011b76f001d5be0b2
Michal Ludvig <mludvig@logix.net.nz>
2009-01-07 03:06:35 +0000

    * S3/ACL.py: Generate XML from a current list of Grantees
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3cmd/trunk@330 830e0280-6d2a-0410-9c65-932aecc39d9d

--- S3/ACL.py
+++ S3/ACL.py
@@ -19,14 +19,30 @@ class Grantee(object):
 	display_name = None
 	permission = None
 
-	def __str__(self):
-		return '%(name)s : %(permission)s' % { "name" : self.name, "permission" : self.permission }
+	def __repr__(self):
+		return 'Grantee("%(tag)s", "%(name)s", "%(permission)s")' % { 
+			"tag" : self.tag, 
+			"name" : self.name, 
+			"permission" : self.permission 
+		}
 
 	def isAllUsers(self):
 		return self.tag == "URI" and self.name == Grantee.ALL_USERS_URI
 	
 	def isAnonRead(self):
 		return self.isAllUsers and self.permission == "READ"
+	
+	def getElement(self):
+		el = ET.Element("Grant")
+		grantee = ET.SubElement(el, "Grantee", { 
+			'xmlns:xsi' : 'http://www.w3.org/2001/XMLSchema-instance',
+			'xsi:type' : self.xsi_type
+		})
+		name = ET.SubElement(grantee, self.tag)
+		name.text = self.name
+		permission = ET.SubElement(el, "Permission")
+		permission.text = self.permission
+		return el
 
 class GranteeAnonRead(Grantee):
 	xsi_type = "Group"
@@ -35,20 +51,15 @@ class GranteeAnonRead(Grantee):
 	permission = "READ"
 
 class ACL(object):
-	EMPTY_ACL = """
-	<AccessControlPolicy>
-		<AccessControlList>
-		</AccessControlList>
-	</AccessControlPolicy>
-	"""
+	EMPTY_ACL = "<AccessControlPolicy><AccessControlList></AccessControlList></AccessControlPolicy>"
 
-	grants = []
+	grantees = []
 
 	def __init__(self, xml = None):
 		if not xml:
 			xml = ACL.EMPTY_ACL
 		self.tree = getTreeFromXml(xml)
-		self.grants = self.parseGrants()
+		self.parseGrants()
 	
 	def parseGrants(self):
 		for grant in self.tree.findall(".//Grant"):
@@ -62,12 +73,11 @@ class ACL(object):
 				else:
 					grantee.tag = el.tag
 					grantee.name = el.text
-			self.grants.append(grantee)
-		return self.grants
+			self.grantees.append(grantee)
 
 	def getGrantList(self):
 		acl = {}
-		for grantee in self.grants:
+		for grantee in self.grantees:
 			if grantee.display_name:
 				user = grantee.display_name
 			elif grantee.isAllUsers():
@@ -78,20 +88,25 @@ class ACL(object):
 		return acl
 
 	def isAnonRead(self):
-		for grantee in self.grants:
+		for grantee in self.grantees:
 			if grantee.isAnonRead():
 				return True
 		return False
 	
 	def grantAnonRead(self):
 		if not self.isAnonRead():
-			self.grants.append(GranteeAnonRead())
+			self.grantees.append(GranteeAnonRead())
 	
 	def revokeAnonRead(self):
-		self.grants = [g for g in self.grants if not g.isAnonRead()]
+		self.grantees = [g for g in self.grantees if not g.isAnonRead()]
 
 	def __str__(self):
-		return ET.tostring(self.tree)
+		tree = getTreeFromXml(ACL.EMPTY_ACL)
+		tree.attrib['xmlns'] = "http://s3.amazonaws.com/doc/2006-03-01/"
+		acl = tree.find(".//AccessControlList")
+		for grantee in self.grantees:
+			acl.append(grantee.getElement())
+		return ET.tostring(tree)
 
 if __name__ == "__main__":
 	xml = """<?xml version="1.0" encoding="UTF-8"?>
@@ -123,4 +138,4 @@ if __name__ == "__main__":
 	print "Grants:", acl.getGrantList()
 	acl.grantAnonRead()
 	print "Grants:", acl.getGrantList()
-	#print acl
+	print acl
