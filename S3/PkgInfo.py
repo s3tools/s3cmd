b39528ea99279557b666878669479db0535bedda
Michal Ludvig <mludvig@logix.net.nz>
2009-01-29 23:57:47 +0000

    * Released version 0.9.9-rc2
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3cmd/trunk@364 830e0280-6d2a-0410-9c65-932aecc39d9d

--- S3/PkgInfo.py
+++ S3/PkgInfo.py
@@ -1,8 +1,8 @@
 package = "s3cmd"
-version = "0.9.9-rc1"
+version = "0.9.9-rc2"
 url = "http://s3tools.logix.cz"
 license = "GPL version 2"
-short_description = "S3cmd is a tool for managing Amazon S3 storage space."
+short_description = "Command line tool for managing Amazon S3 and CloudFront services"
 long_description = """
 S3cmd lets you copy files from/to Amazon S3 
 (Simple Storage Service) using a simple to use
