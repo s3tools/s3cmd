c321acc3c20a6921c2d2d3ef517232cdc68241ec
Michal Ludvig <mludvig@logix.net.nz>
2009-10-06 03:38:35 +0000

    * Released version 0.9.9.90
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3cmd/trunk@398 830e0280-6d2a-0410-9c65-932aecc39d9d

--- S3/PkgInfo.py
+++ S3/PkgInfo.py
@@ -1,5 +1,5 @@
 package = "s3cmd"
-version = "0.9.9"
+version = "0.9.9.90"
 url = "http://s3tools.org"
 license = "GPL version 2"
 short_description = "Command line tool for managing Amazon S3 and CloudFront services"
