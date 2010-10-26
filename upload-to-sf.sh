3db91d0dcdc3c656c8aa9ff2094e768d2459ca19
Michal Ludvig <mludvig@logix.net.nz>
2010-10-26 12:01:20 +0000

    * TODO: Updated. * upload-to-sf.sh: Updated for new SF.net system
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3cmd/trunk@449 830e0280-6d2a-0410-9c65-932aecc39d9d

--- upload-to-sf.sh
+++ upload-to-sf.sh
@@ -2,4 +2,6 @@
 
 VERSION=$(./s3cmd --version | awk '{print $NF}')
 echo -e "Uploading \033[32ms3cmd \033[31m${VERSION}\033[0m ..."
-rsync -avP dist/s3cmd-${VERSION}.* ludvigm@frs.sourceforge.net:uploads/
+#rsync -avP dist/s3cmd-${VERSION}.* ludvigm@frs.sourceforge.net:uploads/
+ln -f NEWS README.txt
+rsync -avP dist/s3cmd-${VERSION}.* README.txt ludvigm,s3tools@frs.sourceforge.net:/home/frs/project/s/s3/s3tools/s3cmd/${VERSION}/
