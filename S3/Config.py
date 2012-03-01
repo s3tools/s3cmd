c42c3f2d1be7a41f31eb5b2c3ab0c2a95a5e5465
Matt Domsch <Matt_Domsch@dell.com>
2012-03-01 20:45:06 +0000

    add --delay-updates option

--- S3/Config.py
+++ S3/Config.py
@@ -54,6 +54,7 @@ class Config(object):
     ]
     delete_removed = False
     _doc['delete_removed'] = "[sync] Remove remote S3 objects when local file has been deleted"
+    delay_updates = False
     gpg_passphrase = ""
     gpg_command = ""
     gpg_encrypt = "%(gpg_command)s -c --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s"
