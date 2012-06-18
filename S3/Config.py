07c9e2de983df3f4ee352b7ac2cdaa643eb6dda8
Matt Domsch <Matt_Domsch@dell.com>
2012-06-18 15:17:32 -0500

    sync: add --add-destination, parallelize uploads to multiple destinations
    Only meaningful at present in the sync local->remote(s) case, this
    adds the --add-destination <foo> command line option.  For the last
    arg (the traditional destination), and each destination specified via
    --add-destination, fork and upload after the initial walk of the local
    file system has completed (and done all the disk I/O to calculate md5
    values for each file).
    
    This keeps us from pounding the file system doing (the same) disk I/O
    for each possible destination, and allows full use of our bandwidth to
    upload in parallel.

--- S3/Config.py
+++ S3/Config.py
@@ -86,6 +86,7 @@ class Config(object):
     website_index = "index.html"
     website_error = ""
     website_endpoint = "http://%(bucket)s.s3-website-%(location)s.amazonaws.com/"
+    additional_destinations = []
 
     ## Creating a singleton
     def __new__(self, configfile = None):
