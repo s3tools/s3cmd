41787acd9d4b59ddc531a3831d2fb86671ea524b
Matt Domsch <matt@domsch.com>
2015-02-03 07:06:23 -0600

    Don't crash when a proxy returns an invalid XML error document
    Give us a chance to see it, and maybe continue.

--- S3/Exceptions.py
+++ S3/Exceptions.py
@@ -98,11 +98,14 @@ class S3Error (S3Exception):
         error_node = tree
         if not error_node.tag == "Error":
             error_node = tree.find(".//Error")
-        for child in error_node.getchildren():
-            if child.text != "":
-                debug("ErrorXML: " + child.tag + ": " + repr(child.text))
-                info[child.tag] = child.text
-
+            if error_node is not None:
+                for child in error_node.getchildren():
+                    if child.text != "":
+                        debug("ErrorXML: " + child.tag + ": " + repr(child.text))
+                        info[child.tag] = child.text
+            else:
+                warning("Malformed error XML returned from remote server.  Use --debug to see it.")
+                debug("ErrorXML: " + repr(tree))
         return info
 
 
