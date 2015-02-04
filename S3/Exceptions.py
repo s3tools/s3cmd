s3tools/s3cmd

commit 41787acd9d4b59ddc531a3831d2fb86671ea524b

Don't crash when a proxy returns an invalid XML error document
Give us a chance to see it, and maybe continue.

S3/Exceptions.py
@@ -98,11 +98,14 @@ class S3Error (S3Exception):
         error_node = tree
         if not error_node.tag == "Error":
             error_node = tree.find(".//Error")
-        for child in error_node.getchildren():
-            if child.text != "":
-                debug("ErrorXML: " + child.tag + ": " + repr(child.text))
-                info[child.tag] = child.text
-
+            if error_node is not None:
+                for child in error_node.getchildren():
+                    if child.text != "":
+                        debug("ErrorXML: " + child.tag + ": " + repr(child.text))
+                        info[child.tag] = child.text
+            else:
+                warning("Malformed error XML returned from remote server.  Use --debug to see it.")
+                debug("ErrorXML: " + repr(tree))
         return info
 
 

commit dd1f7fb8d1747c06399236db48b8b50c7275a8f8

Fix indentation causing failures
Commit 41787acd9 accidentally over-indented its change.  This was
leading to test suite failures, valid failures.  Specifically, the
test suite would fail when removing buckets that have objects in them
(the first test of the suite).

Reading that patch, it's obvious that the "if error_node is not None"
test got indented one level too deep from where it was supposed to
be.  Fixing that resolves the test suite failure again.

S3/Exceptions.py
@@ -101,13 +101,13 @@ class S3Error (S3Exception):
         error_node = tree
         if not error_node.tag == "Error":
             error_node = tree.find(".//Error")
-            if error_node is not None:
-                for child in error_node.getchildren():
-                    if child.text != "":
-                        debug("ErrorXML: " + child.tag + ": " + repr(child.text))
-                        info[child.tag] = child.text
-            else:
-                raise S3ResponseError("Malformed error XML returned from remote server.")
+        if error_node is not None:
+            for child in error_node.getchildren():
+                if child.text != "":
+                    debug("ErrorXML: " + child.tag + ": " + repr(child.text))
+                    info[child.tag] = child.text
+        else:
+            raise S3ResponseError("Malformed error XML returned from remote server.")
         return info
 
 