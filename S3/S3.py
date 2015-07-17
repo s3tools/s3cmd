5e6d61f228ada071e8fdd750ec2042dd5de57b6c
Bret Curtis <psi29a@gmail.com>
2015-07-17 12:50:08 +0200

    local variable 'conn' referenced before assignment

--- S3/S3.py
+++ S3/S3.py
@@ -1029,8 +1029,6 @@ class S3(object):
             if hasattr(e, 'errno') and e.errno != errno.EPIPE:
                 raise
             # close the connection and re-establish
-            conn.counter = ConnMan.conn_max_counter
-            ConnMan.put(conn)
             if retries:
                 warning("Retrying failed request: %s (%s)" % (resource['uri'], e))
                 warning("Waiting %d sec..." % self._fail_wait(retries))
