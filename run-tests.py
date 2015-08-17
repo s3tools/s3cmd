5fd8fd45f4302eb616d18874be88fb26573e2651
Matt Domsch <matt@domsch.com>
2015-08-16 23:11:45 -0500

    Fix Rename (NoSuchKey) test string tested for
    The string being tested for changed in s3cmd.  Update the test suite
    to match the string being output by s3cmd.

--- run-tests.py
+++ run-tests.py
@@ -464,7 +464,7 @@ test_s3cmd("Rename within S3", ['mv', '%s/xyz/etc/logo.png' % pbucket(1), '%s/xy
 ## ====== Rename (NoSuchKey)
 test_s3cmd("Rename (NoSuchKey)", ['mv', '%s/xyz/etc/logo.png' % pbucket(1), '%s/xyz/etc2/Logo.PNG' % pbucket(1)],
     retcode = EX_NOTFOUND,
-    must_find_re = [ 'ERROR:.*NoSuchKey' ],
+    must_find_re = [ 'Key not found' ],
     must_not_find = [ 'File %s/xyz/etc/logo.png moved to %s/xyz/etc2/Logo.PNG' % (pbucket(1), pbucket(1)) ])
 
 ## ====== Sync more from S3 (invalid src)
