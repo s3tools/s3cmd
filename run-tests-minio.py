38f1561c159af3842071d89fa77df25a17250410
Shaform <shaform@gmail.com>
2016-12-08 07:51:42 +0800

    run python3 for run-tests.py

--- run-tests-minio.py
+++ run-tests-minio.py
@@ -7,7 +7,7 @@
 ## License: GPL Version 2
 ## Copyright: TGRMN Software and contributors
 
-from __future__ import print_function
+from __future__ import absolute_import, print_function
 
 import sys
 import os
