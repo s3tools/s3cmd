f48d0a0c5d115214b835d57753e0d24b039c4bd0
Andrew Gaul <andrew@gaul.org>
2017-10-29 17:51:32 -0700

    Add config to avoid checking MD5 on get and put
    Overload existing --no-check-md5.  Object stores like S3Proxy cannot
    return an MD5 ETag using some providers:
    
    * Atmos returns nothing
    * Azure returns an opaque ETag
    * B2 returns a SHA1

--- S3/Config.py
+++ S3/Config.py
@@ -206,6 +206,7 @@ class Config(object):
     # s3 will timeout if a request/transfer is stuck for more than a short time
     throttle_max = 100
     public_url_use_https = False
+    put_check_etag = True
 
     ## Creating a singleton
     def __new__(self, configfile = None, access_key=None, secret_key=None, access_token=None):
