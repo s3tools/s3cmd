5015af3cb45fc911fb0cfe457bc9ddf577df4bc7
Florent Viard <florent@sodria.com>
2020-03-15 15:10:36 +0100

    setup.py: update meta files with support for recent python versions

--- setup.py
+++ setup.py
@@ -105,6 +105,8 @@ Authors:
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: System :: Archiving',
         'Topic :: Utilities',
     ],
